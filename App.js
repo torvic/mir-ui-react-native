861fa339284d3aadaa0783f07bd73c0e16d46137
torvic <130111@unsaac.edu.pe>
2022-02-18 18:49:42 -0500

    Add sample data and test it

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { comments } from './src/comments';
 
 export default function App() {
+	console.log('comentarios: ', comments);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
