3b806b12822485ff0871abbc4bed3e9f84ee09b1
torvic <130111@unsaac.edu.pe>
2022-02-18 19:52:06 -0500

    Add Card component & use it in App.js

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,22 @@
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Image, Text } from 'react-native'
 
-const Card = () => {
+const Card = ({ data }) => {
+  console.log('data', data)
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.image}
-        source={{ uri: 'http://placehold.it/32x32' }}
-      />
-      <View>
-        <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
-          nemo!
-        </Text>
-        <Text>by Fram Smith</Text>
-        <View>
-          <Text>LABEL 1</Text>
+      <View style={styles.figure}>
+        <Image
+          style={styles.image}
+          source={{ uri: 'http://placehold.it/32x32' }}
+        />
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.title}>{data.about.slice(0, 30)}</Text>
+        <Text style={styles.name}>{`by ${data.author}`}</Text>
+        <View style={styles.label}>
+          <Text style={styles.labelText}>LABEL 1</Text>
         </View>
-        <Text>5 comments</Text>
+        <Text style={styles.comment}>5 comments</Text>
       </View>
     </View>
   )
@@ -24,11 +24,46 @@ const Card = () => {
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 50,
+    flex: 1,
+    flexDirection: 'row',
+    paddingTop: 30,
+    borderBottomColor: 'black',
+    borderWidth: 1,
+  },
+  figure: {
+    flex: 1,
+    width: '50%',
+    height: 150,
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
   image: {
-    width: 66,
-    height: 58,
+    flex: 1,
+    height: '100%',
+    padding: 10,
+  },
+  content: {
+    flex: 1,
+    padding: 10,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 12,
+  },
+  name: {
+    fontSize: 12,
+  },
+  label: {
+    padding: 3,
+    backgroundColor: 'gray',
+  },
+  labelText: {
+    fontSize: 8,
+    color: 'white',
+  },
+  comment: {
+    fontSize: 10,
   },
 })
 
